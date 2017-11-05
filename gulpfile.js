73d03deed23de4fb769a9b4614711aff5a84f1a3
Marco Biedermann <marco.biedermann@gmx.de>
2017-11-05 19:07:50 +0100

    fix eslint errors and clean up code

--- gulpfile.js
+++ gulpfile.js
@@ -1,142 +1,118 @@
-const babelify          = require('babelify');
-const browserify        = require('browserify');
-const fs                = require('fs');
-const gulp              = require('gulp');
-const gulpCleanCss      = require('gulp-clean-css');
-const gulpEjs           = require('gulp-ejs');
-const gulpEslint        = require('gulp-eslint');
-const gulpHtmlmin       = require('gulp-htmlmin');
-const gulpImagemin      = require('gulp-imagemin');
-const gulpJsonlint      = require('gulp-jsonlint');
-const gulpPostcss       = require('gulp-postcss');
-const gulpRev           = require('gulp-rev');
-const gulpSourcemaps    = require('gulp-sourcemaps');
-const gulpSvgmin        = require('gulp-svgmin');
-const gulpSvgstore      = require('gulp-svgstore');
-const gulpUglify        = require('gulp-uglify');
-const gulpWebp          = require('gulp-webp');
-const path              = require('path');
-const postcssCssnext    = require('postcss-cssnext');
-const postcssImport     = require('postcss-import');
-// const postcssModules    = require('postcss-modules');
-const postcssReporter   = require('postcss-reporter');
-const stylelint         = require('stylelint');
-const vinylBuffer       = require('vinyl-buffer');
+const babelify = require('babelify');
+const browserify = require('browserify');
+const fs = require('fs');
+const gulp = require('gulp');
+const gulpCleanCss = require('gulp-clean-css');
+const gulpEjs = require('gulp-ejs');
+const gulpEslint = require('gulp-eslint');
+const gulpHtmlmin = require('gulp-htmlmin');
+const gulpImagemin = require('gulp-imagemin');
+const gulpJsonlint = require('gulp-jsonlint');
+const gulpPostcss = require('gulp-postcss');
+const gulpRev = require('gulp-rev');
+const gulpSourcemaps = require('gulp-sourcemaps');
+const gulpSvgmin = require('gulp-svgmin');
+const gulpSvgstore = require('gulp-svgstore');
+const gulpUglify = require('gulp-uglify');
+const gulpWebp = require('gulp-webp');
+const path = require('path');
+const postcssCssnext = require('postcss-cssnext');
+const postcssImport = require('postcss-import');
+const postcssReporter = require('postcss-reporter');
+const stylelint = require('stylelint');
+const vinylBuffer = require('vinyl-buffer');
 const vinylSourceStream = require('vinyl-source-stream');
 
 const dirs = {
   source: './source',
-  dest  : './docs'
+  dest: './docs',
 };
-
-function getJSONFromCssModules(cssFileName, json) {
-  const cssName      = path.basename(cssFileName, '.css');
-  const jsonFileName = path.resolve(`${dirs.dest}/assets/css`, `${cssName}.json`);
-
-  fs.writeFileSync(jsonFileName, JSON.stringify(json));
-}
 
 function getClass(module, className) {
   return JSON.parse(fs.readFileSync(path.resolve(`${dirs.dest}/assets/css`, `${module}.json`)).toString())[className];
 }
 
-function getRev() {
-  return JSON.parse(fs.readFileSync(`${dirs.dest}/assets/rev-manifest.json`));
+function getJSON(file) {
+  return JSON.parse(fs.readFileSync(file, 'utf-8'));
 }
 
-gulp.task('copy', () => {
-  return gulp.src([
-    `${dirs.source}/**/*.txt`,
-    `${dirs.source}/**/*.xml`,
-    `${dirs.source}/CNAME`,
-    `${dirs.source}/apple-touch-icon-precomposed.png`,
-    `${dirs.source}/favicon.ico`,
-    `${dirs.source}/favicon.svg`,
-    `${dirs.source}/manifest.json`,
-    `${dirs.source}/tile-wide.png`,
-    `${dirs.source}/tile.png`
-  ])
-  .pipe(gulp.dest(`${dirs.dest}`));
-});
+gulp.task('copy', () => gulp.src([
+  `${dirs.source}/**/*.txt`,
+  `${dirs.source}/**/*.xml`,
+  `${dirs.source}/CNAME`,
+  `${dirs.source}/apple-touch-icon-precomposed.png`,
+  `${dirs.source}/favicon.ico`,
+  `${dirs.source}/favicon.svg`,
+  `${dirs.source}/manifest.json`,
+  `${dirs.source}/tile-wide.png`,
+  `${dirs.source}/tile.png`,
+])
+  .pipe(gulp.dest(`${dirs.dest}`)));
 
-gulp.task('css', () => {
-  return gulp.src(`${dirs.source}/assets/css/style.css`)
-    .pipe(gulpSourcemaps.init())
-    .pipe(gulpPostcss([
-      postcssImport(),
-      postcssCssnext({
-        features: {
-          rem: false
-        }
-      })
-      // ,postcssModules({
-      //   getJSON: getJSONFromCssModules,
-      //   generateScopedName: '[emoji]'
-      // })
-    ]))
-    .pipe(gulpCleanCss())
-    .pipe(gulpRev())
-    .pipe(gulpSourcemaps.write('.'))
-    .pipe(gulp.dest(`${dirs.dest}/assets/css`))
-    .pipe(gulpRev.manifest(`${dirs.dest}/assets/rev-manifest.json`, {
-      base : process.cwd() + `${dirs.dest}/assets`,
-      merge: true
-    }))
-    .pipe(gulp.dest(`${dirs.dest}/assets`));
-});
+gulp.task('css', () => gulp.src(`${dirs.source}/assets/css/style.css`)
+  .pipe(gulpSourcemaps.init())
+  .pipe(gulpPostcss([
+    postcssImport(),
+    postcssCssnext({
+      features: {
+        rem: false,
+      },
+    }),
+  ]))
+  .pipe(gulpCleanCss())
+  .pipe(gulpRev())
+  .pipe(gulpSourcemaps.write('.'))
+  .pipe(gulp.dest(`${dirs.dest}/assets/css`))
+  .pipe(gulpRev.manifest(`${dirs.dest}/assets/rev-manifest.json`, {
+    base: `${process.cwd()}${dirs.dest}/assets`,
+    merge: true,
+  }))
+  .pipe(gulp.dest(`${dirs.dest}/assets`)));
 
-gulp.task('fonts', () => {
-  return gulp.src(`${dirs.source}/assets/fonts/**/*.{woff,woff2}`)
-    .pipe(gulp.dest(`${dirs.dest}/assets/fonts`));
-});
+gulp.task('fonts', () => gulp.src(`${dirs.source}/assets/fonts/**/*.{woff,woff2}`)
+  .pipe(gulp.dest(`${dirs.dest}/assets/fonts`)));
 
-gulp.task('html', ['css', 'js', 'svg:icons'], () => {
-  return gulp.src(`${dirs.source}/templates/pages/**/*.ejs`)
-    .pipe(gulpEjs({
-      className: getClass,
-      data     : require(`${dirs.source}/data/projects.json`),
-      rev      : getRev()
-    }, {}, {
-      ext: '.html'
-    }))
-    .pipe(gulpHtmlmin({
-      caseSensitive                : true,
-      collapseBooleanAttributes    : true,
-      collapseWhitespace           : true,
-      minifyCSS                    : true,
-      minifyJS                     : true,
-      minifyURLs                   : false,
-      removeAttributeQuotes        : true,
-      removeCDATASectionsFromCDATA : true,
-      removeComments               : true,
-      removeCommentsFromCDATA      : true,
-      removeEmptyAttributes        : true,
-      removeOptionalTags           : true,
-      removeRedundantAttributes    : true,
-      removeScriptTypeAttributes   : true,
-      removeStyleLinkTypeAttributes: true,
-      useShortDoctype              : true
-    }))
-    .pipe(gulp.dest(`${dirs.dest}`));
-});
+gulp.task('html', ['css', 'js', 'svg:icons'], () => gulp.src(`${dirs.source}/templates/pages/**/*.ejs`)
+  .pipe(gulpEjs({
+    className: getClass,
+    data: getJSON(`${dirs.source}/data/projects.json`),
+    rev: getJSON(`${dirs.dest}/assets/rev-manifest.json`),
+  }, {}, {
+    ext: '.html',
+  }))
+  .pipe(gulpHtmlmin({
+    caseSensitive: true,
+    collapseBooleanAttributes: true,
+    collapseWhitespace: true,
+    minifyCSS: true,
+    minifyJS: true,
+    minifyURLs: false,
+    removeAttributeQuotes: true,
+    removeCDATASectionsFromCDATA: true,
+    removeComments: true,
+    removeCommentsFromCDATA: true,
+    removeEmptyAttributes: true,
+    removeOptionalTags: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    useShortDoctype: true,
+  }))
+  .pipe(gulp.dest(`${dirs.dest}`)));
 
-gulp.task('images:content', () => {
-  return gulp.src(`${dirs.source}/content/images/**/*.{gif,ico,jpg,jpeg,png}`)
-    .pipe(gulpImagemin())
-    .pipe(gulp.dest(`${dirs.dest}/content/images`));
-});
+gulp.task('images:content', () => gulp.src(`${dirs.source}/content/images/**/*.{gif,ico,jpg,jpeg,png}`)
+  .pipe(gulpImagemin())
+  .pipe(gulp.dest(`${dirs.dest}/content/images`)));
 
-gulp.task('images:webp', () => {
-  return gulp.src(`${dirs.source}/content/images/**/*.{jpg,jpeg,gif,png}`)
-    .pipe(gulpWebp())
-    .pipe(gulp.dest(`${dirs.dest}/content/images`));
-});
+gulp.task('images:webp', () => gulp.src(`${dirs.source}/content/images/**/*.{jpg,jpeg,gif,png}`)
+  .pipe(gulpWebp())
+  .pipe(gulp.dest(`${dirs.dest}/content/images`)));
 
 gulp.task('js', () => {
   const b = browserify({
-    entries  : `${dirs.source}/assets/js/script.js`,
-    debug    : true,
-    transform: [babelify]
+    entries: `${dirs.source}/assets/js/script.js`,
+    debug: true,
+    transform: [babelify],
   });
 
   return b.bundle()
@@ -148,55 +124,43 @@ gulp.task('js', () => {
     .pipe(gulpSourcemaps.write('.'))
     .pipe(gulp.dest(`${dirs.dest}/assets/js`))
     .pipe(gulpRev.manifest(`${dirs.dest}/assets/rev-manifest.json`, {
-      base : process.cwd() + `${dirs.dest}/assets`,
-      merge: true
+      base: `${process.cwd()}${dirs.dest}/assets`,
+      merge: true,
     }))
     .pipe(gulp.dest(`${dirs.dest}/assets`));
 });
 
-gulp.task('lint:css', () => {
-  return gulp.src(`${dirs.source}/assets/css/**/*.css`)
-    .pipe(gulpPostcss([
-      stylelint(),
-      postcssReporter()
-    ]));
-});
+gulp.task('lint:css', () => gulp.src(`${dirs.source}/assets/css/**/*.css`)
+  .pipe(gulpPostcss([
+    stylelint(),
+    postcssReporter(),
+  ])));
 
-gulp.task('lint:js', () => {
-  return gulp.src(`${dirs.source}/assets/js/**/*.js`)
-    .pipe(gulpEslint());
-});
+gulp.task('lint:js', () => gulp.src(`${dirs.source}/assets/js/**/*.js`)
+  .pipe(gulpEslint()));
 
-gulp.task('lint:json', () => {
-  return gulp.src(`${dirs.source}/**/*.json`)
-    .pipe(gulpJsonlint())
-    .pipe(gulpJsonlint.reporter());
-});
+gulp.task('lint:json', () => gulp.src(`${dirs.source}/**/*.json`)
+  .pipe(gulpJsonlint())
+  .pipe(gulpJsonlint.reporter()));
 
-gulp.task('svg:assets', () => {
-  return gulp.src(`${dirs.source}/assets/images/*.svg`)
-    .pipe(gulpSvgmin())
-    .pipe(gulp.dest(`${dirs.dest}/assets/images`));
-});
+gulp.task('svg:assets', () => gulp.src(`${dirs.source}/assets/images/*.svg`)
+  .pipe(gulpSvgmin())
+  .pipe(gulp.dest(`${dirs.dest}/assets/images`)));
 
-gulp.task('svg:content', () => {
-  return gulp.src(`${dirs.source}/content/images/**/*.svg`)
-    .pipe(gulpSvgmin())
-    .pipe(gulp.dest(`${dirs.dest}/content/images`));
-});
+gulp.task('svg:content', () => gulp.src(`${dirs.source}/content/images/**/*.svg`)
+  .pipe(gulpSvgmin())
+  .pipe(gulp.dest(`${dirs.dest}/content/images`)));
 
-gulp.task('svg:icons', () => {
-  return gulp.src(`${dirs.source}/assets/images/icons/**/*.svg`)
-    .pipe(gulpSvgmin())
-    .pipe(gulpSvgstore())
-    .pipe(gulpRev())
-    .pipe(gulp.dest(`${dirs.dest}/assets/images`))
-    .pipe(gulpRev.manifest(`${dirs.dest}/assets/rev-manifest.json`, {
-      base : process.cwd() + `${dirs.dest}/assets`,
-      merge: true
-    }))
-    .pipe(gulp.dest(`${dirs.dest}/assets`));
-});
+gulp.task('svg:icons', () => gulp.src(`${dirs.source}/assets/images/icons/**/*.svg`)
+  .pipe(gulpSvgmin())
+  .pipe(gulpSvgstore())
+  .pipe(gulpRev())
+  .pipe(gulp.dest(`${dirs.dest}/assets/images`))
+  .pipe(gulpRev.manifest(`${dirs.dest}/assets/rev-manifest.json`, {
+    base: `${process.cwd()}${dirs.dest}/assets`,
+    merge: true,
+  }))
+  .pipe(gulp.dest(`${dirs.dest}/assets`)));
 
 gulp.task('watch', () => {
   gulp.watch(`${dirs.source}/data/**/*.json`, ['html']);
@@ -208,13 +172,13 @@ gulp.task('watch', () => {
 
 gulp.task('images', [
   'images:content',
-  'images:webp'
+  'images:webp',
 ]);
 
 gulp.task('svg', [
   'svg:assets',
   'svg:content',
-  'svg:icons'
+  'svg:icons',
 ]);
 
 gulp.task('default', [
@@ -224,7 +188,7 @@ gulp.task('default', [
   'images',
   'svg:assets',
   'svg:content',
-  'watch'
+  'watch',
 ]);
 
 gulp.task('build', [
@@ -232,11 +196,11 @@ gulp.task('build', [
   'fonts',
   // 'images',
   'svg:assets',
-  'svg:content'
+  'svg:content',
 ]);
 
 gulp.task('lint', [
   'lint:css',
   'lint:js',
-  'lint:json'
+  'lint:json',
 ]);
