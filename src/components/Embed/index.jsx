1f429374e6c20c6c15ce9373ba3576a0f7fd3fb3
Marco Biedermann <marco.biedermann@gmx.de>
2017-12-17 22:25:48 +0100

    add Embed component

---
+++ src/components/Embed/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import './style.css';
+
+const Embed = (props) => {
+  const className = classNames({
+    embed: true,
+    [`embed--${props.ratio}`]: props.ratio,
+  });
+
+  return (
+    <div className={className}>
+      {props.children}
+    </div>
+  );
+};
+
+Embed.propTypes = {
+  children: PropTypes.node.isRequired,
+  ratio: PropTypes.string,
+};
+
+Embed.defaultProps = {
+  ratio: '',
+};
+
+export default Embed;
