02dae39912d150951aacd0865827455fa5c487fa
Marco Biedermann <marco.biedermann@gmx.de>
2017-12-17 13:53:22 +0100

    add error 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
+
+import Grid from '../components/Grid';
 
 const Page = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Helmet
+      title="404 - Page not found"
+      meta={[
+        {
+          name: 'description',
+          content: 'Error 404 - Page not found',
+        },
+      ]}
+    />
+    <section className="site__section">
+
+      <Grid>
+
+        <h1>Error 404 - Page not found</h1>
+        <p>The requested page could not be found.<br />Go back to <Link to="/">Home page</Link></p>
+
+      </Grid>
+
+    </section>
   </div>
 );
 
