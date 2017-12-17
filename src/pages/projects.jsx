530da4c369387532479ffa51f386360814d15621
Marco Biedermann <marco.biedermann@gmx.de>
2017-12-17 21:37:11 +0100

    add projects page

---
+++ src/pages/projects.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+import Grid from '../components/Grid';
+
+const Page = () => (
+  <div>
+    <Helmet
+      title="Projects"
+      meta={[
+        {
+          name: 'description',
+          content: 'Take a look at some of my projects I’ve been working on.',
+        },
+      ]}
+    />
+    <section className="site__section">
+
+      <Grid>
+
+        <h2>Work</h2>
+        <p className="center-block">Take a look at some of my projects I’ve been working on.</p>
+        PROJECTS
+
+      </Grid>
+
+    </section>
+  </div>
+);
+
+export default Page;
