8bed6be7e1805509aeb65467c21327d1fe9e1212
Marco Biedermann <marco.biedermann@gmx.de>
2017-12-17 22:55:51 +0100

    add gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,29 @@ module.exports = {
     title: 'Gatsby Default Starter',
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'marcobiedermann',
+        short_name: 'marcobiedermann',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#000',
+        display: 'minimal-ui',
+        icons: [
+          {
+            src: '/favicons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/favicons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    },
     'gatsby-plugin-offline',
     'gatsby-plugin-react-helmet',
   ],
