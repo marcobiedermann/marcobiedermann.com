8d1153f5ea06e2f2f7a796db4baba022baffaa6e
Marco Biedermann <marco.biedermann@gmx.de>
2017-12-25 17:42:03 +0100

    add Image component

---
+++ src/components/Image/index.jsx
@@ -0,0 +1,51 @@
+import lozad from 'lozad';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './style.css';
+
+class Image extends Component {
+  constructor(props) {
+    super(props);
+    this.observer = lozad();
+  }
+
+  componentDidMount() {
+    this.observer.observe();
+  }
+
+  render() {
+    const { props } = this;
+
+    return (
+      <picture>
+        <source
+          data-srcset=""
+          type="image/webp"
+        />
+        <img
+          alt={props.alt}
+          className="lozad"
+          data-src={props.src}
+          height={props.height}
+          width={props.width}
+        />
+      </picture>
+    );
+  }
+}
+
+Image.propTypes = {
+  alt: PropTypes.string,
+  height: PropTypes.number,
+  src: PropTypes.string.isRequired,
+  width: PropTypes.number,
+};
+
+Image.defaultProps = {
+  alt: '',
+  height: null,
+  width: null,
+};
+
+export default Image;
